219c9ba17496e4b6f7833756a66fa480e260256d
MMD <sirmmds@gmail.com>
2018-09-01 16:29:17 +0430

    fix webpack prod config and build assets

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,16 +53,14 @@ const webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap ?
-        {
-          safe: true,
-          map: {
-            inline: false
-          }
-        } :
-        {
-          safe: true
+      cssProcessorOptions: config.build.productionSourceMap ? {
+        safe: true,
+        map: {
+          inline: false
         }
+      } : {
+        safe: true
+      }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
@@ -116,13 +114,20 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
 
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, '../static'),
+      to: config.build.assetsSubDirectory,
+      ignore: ['.*']
+    }]),
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new webpack.ProvidePlugin({
+      Popper: 'popper.js',
+    })
   ]
 })
 
@@ -141,15 +146,6 @@ if (config.build.productionGzip) {
       threshold: 10240,
       minRatio: 0.8
     }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jquery: 'jquery',
-      'window.jQuery': 'jquery',
-      jQuery: 'jquery'
-    }),
-    new webpack.ProvidePlugin({
-      Popper: 'popper.js',
-    })
   )
 }
 
