9e68bfd9af2ea0f72fc2899270ef953b2b3f6e9c
MMD <sirmmds@gmail.com>
2019-04-12 10:03:34 +0430

    update endpoint

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -123,7 +123,7 @@ Vue.mixin({
       if (this.isDev)
         return "http://localhost:8000/" + url;
       // else return "http://api.sh.mmdmst.ir/" + url;
-      else return "http://mmdmst.ir:8080/" + url;
+      else return "http://api." + window.location.hostname + "/" + url;
     },
     setToken(token) {
       localStorage.setItem('token', token);
