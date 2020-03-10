b93c4edc8a0a1ec19aab02868865a17f08cc5b54
MMD <sirmmds@gmail.com>
2020-03-10 07:13:03 +0330

    refactor balances

---
+++ src/components/panel/balance/_datatableBaseCols.js
@@ -0,0 +1,27 @@
+export default {
+  cols: [{
+      th: "جمع بدهکار",
+      td: "bed_sum",
+      type: "money",
+      filters: ["bed_sum"]
+    },
+    {
+      th: "جمع بستانکار",
+      td: "bes_sum",
+      type: "money",
+      filters: ["bes_sum"]
+    },
+    {
+      th: "مانده بدهکار",
+      td: "bed_remain",
+      type: "money",
+      filters: ["bed_remain"]
+    },
+    {
+      th: "مانده بستانکار",
+      td: "bes_remain",
+      type: "money",
+      filters: ["bes_remain"]
+    },
+  ],
+}
--- src/components/panel/balance/floatAccountsByGroup/_datatableCols.js
+++ src/components/panel/balance/floatAccountsByGroup/_datatableCols.js
@@ -12,29 +12,5 @@ export default {
       type: "text",
       filters: ["float_account_name"]
     },
-    {
-      th: "جمع بدهکار",
-      td: "bed_sum",
-      type: "money",
-      filters: ["bed_sum"]
-    },
-    {
-      th: "جمع بستانکار",
-      td: "bes_sum",
-      type: "money",
-      filters: ["bes_sum"]
-    },
-    {
-      th: "مانده بدهکار",
-      td: "bed_remain",
-      type: "money",
-      filters: ["bed_remain"]
-    },
-    {
-      th: "مانده بستانکار",
-      td: "bes_remain",
-      type: "money",
-      filters: ["bes_remain"]
-    },
   ],
 }
