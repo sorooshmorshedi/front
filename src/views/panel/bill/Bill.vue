a13be1258502035e45476b9941429c89c356e578
MMD <sirmmds@gmail.com>
2020-06-04 15:22:13 +0430

    redesign bill

--- src/views/panel/bill/Bill.vue
+++ src/views/panel/bill/Bill.vue
@@ -1,49 +1,23 @@
 <template>
-  <div class="row rtl">
-    <div class="col-12">
-      <div class="card right">
-        <div class="card-body">
-          <div class="print-header d-none d-print-block">
-            <h2>{{ company.name }}</h2>
-            <span class="float-left">تاریخ چاپ: {{ todayDate() }}</span>
-          </div>
-          <div class="title">
-            صورت حساب
-            <span v-if="account" class="d-none d-print-inline-block">{{ account.name }}</span>
-            <span class="d-print-none">
-              <a @click.prevent="print" href="#/" class="btn btn-info">چاپ</a>
-            </span>
-          </div>
-          <div class="row ledger">
-            <div class="col-lg-12">
-              <div class="card right">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-lg-6 d-print-none">
-                      <label>حساب</label>
-                      <multiselect
-                        dir="rtl"
-                        :options="accountsSelectValues.levels[3]"
-                        v-model="account"
-                        track-by="id"
-                        label="title"
-                        @select="getBill"
-                        id="acc-select"
-                      />
-                    </div>
-
-                    <div class="col-lg-12">
-                      <datatable v-if="account" :cols="datatableCols.cols" :data="rows"/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <v-card>
+    <v-card-title>صورت حساب</v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col cols="12">
+          <account-select
+            label="حساب"
+            :depp-select="false"
+            items-type="level3"
+            v-model="account"
+            @input="getBill"
+          />
+        </v-col>
+        <v-col cols="12">
+          <datatable v-if="account" :cols="datatableCols.cols" :data="rows" />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
