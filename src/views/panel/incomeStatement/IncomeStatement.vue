3136a4eb31e5511e8a65e17f93b3d29be5b29f5e
MMD <sirmmds@gmail.com>
2020-06-04 15:59:14 +0430

    redesign income statement

--- src/views/panel/incomeStatement/IncomeStatement.vue
+++ src/views/panel/incomeStatement/IncomeStatement.vue
@@ -1,94 +1,72 @@
 <template>
-  <div class="row rtl">
-    <div class="col-12">
-      <div class="card right">
-        <div class="card-body">
-          <div class="title">صورت سود و زیان</div>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="card right">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="form-group col-lg-2">
-                      <label>از تاریخ</label>
-                      <date
-                        class="form-control"
-                        v-model="filters.from_date"
-                        placeholder="xxxx-xx-xx"
-                      />
-                    </div>
-                    <div class="form-group col-lg-2">
-                      <label>تا تاریخ</label>
-                      <date
-                        class="form-control"
-                        v-model="filters.to_date"
-                        placeholder="xxxx-xx-xx"
-                      />
-                    </div>
-                    <div class="form-group col-lg-2">
-                      <button @click="getData()" class="btn btn-info btn-label-margin w-100px">گزارش</button>
-                    </div>
-                    <div class="col-lg-12">
-                      <div class="table-responsive table-bordered table-striped">
-                        <table class="table">
-                          <thead>
-                            <tr>
-                              <th>#</th>
-                              <th>شرح</th>
-                              <th colspan="2">مبلغ</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <template v-for="(r, i) in rows">
-                              <tr :key="i">
-                                <td>{{ i+1 }}</td>
-                                <td :class="{'text-bold': i == rows.length-1}">
-                                  <span
-                                    :class="{'text-danger': r.type.prefixColor == 'red', 'text-primary': r.type.prefixColor == 'blue'}"
-                                    v-if="r.type.hasPrefix"
-                                  >{{ r.type.prefix }}</span>
-                                  {{ r.type.name }}
-                                </td>
+  <v-card>
+    <v-card-title>صورت سود و زیان</v-card-title>
 
-                                <td
-                                  :class="{'text-bold': i == rows.length-1}"
-                                  dir="ltr"
-                                >{{ r.remain | toMoney }}</td>
-                                <td v-if="detailed"></td>
-                              </tr>
-                              <template v-if="detailed">
-                                <template v-for="(acc, j) in r.accounts">
-                                  <tr :key="i + '' + j">
-                                    <td></td>
-                                    <td class="account-row">{{ acc.title }}</td>
-                                    <td>{{ acc.remain | toMoney }}</td>
-                                    <td></td>
-                                  </tr>
-                                </template>
-                                <tr :key="i">
-                                  <td></td>
-                                  <td class="account-row">جمع {{ r.type.name }}</td>
-                                  <td></td>
-                                  <td
-                                    :class="{'text-bold': i == rows.length-1}"
-                                    dir="ltr"
-                                  >{{ r.remain | toMoney }}</td>
-                                </tr>
-                              </template>
-                            </template>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <v-card-text>
+      <v-row>
+        <v-col cols="12" md="2">
+          <date label="از تاریخ" v-model="filters.from_date" placeholder="xxxx-xx-xx" />
+        </v-col>
+        <v-col cols="12" md="2">
+          <date label="تا تاریخ" v-model="filters.to_date" placeholder="xxxx-xx-xx" />
+        </v-col>
+
+        <v-col cols="12" md="2">
+          <v-btn @click="getData()" class="blue white--text w-100px mt-1">گزارش</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-simple-table dense>
+            <thead class="grey lighten-3">
+              <tr>
+                <th>#</th>
+                <th>شرح</th>
+                <th colspan="2">مبلغ</th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-for="(r, i) in rows">
+                <tr :key="i">
+                  <td>{{ i+1 }}</td>
+                  <td :class="{'font-weight-bold': i == rows.length-1}">
+                    <span
+                      :class="{'red--text': r.type.prefixColor == 'red', 'blue--text': r.type.prefixColor == 'blue'}"
+                      v-if="r.type.hasPrefix"
+                    >{{ r.type.prefix }}</span>
+                    {{ r.type.name }}
+                  </td>
+
+                  <td
+                    :class="{'font-weight-bold': i == rows.length-1}"
+                    dir="ltr"
+                  >{{ r.remain | toMoney }}</td>
+                  <td v-if="detailed"></td>
+                </tr>
+                <template v-if="detailed">
+                  <template v-for="(acc, j) in r.accounts">
+                    <tr :key="i + '' + j">
+                      <td></td>
+                      <td class="account-row">{{ acc.title }}</td>
+                      <td class="ltr">{{ acc.remain | toMoney }}</td>
+                      <td></td>
+                    </tr>
+                  </template>
+                  <tr :key="'s' + i">
+                    <td></td>
+                    <td class="account-row">جمع {{ r.type.name }}</td>
+                    <td></td>
+                    <td
+                      :class="{'text-bold': i == rows.length-1}"
+                      dir="ltr"
+                    >{{ r.remain | toMoney }}</td>
+                  </tr>
+                </template>
+              </template>
+            </tbody>
+          </v-simple-table>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
