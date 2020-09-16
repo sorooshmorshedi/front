a4a367beabcbacf5af63c48e2f3477832dfa3af9
MMD <sirmmds@gmail.com>
2020-09-16 11:59:17 +0430

    refactor revoke lading bill number

--- src/modules/dashtbashi/RevokeLadingBillNumber.vue
+++ src/modules/dashtbashi/RevokeLadingBillNumber.vue
@@ -1,99 +1,114 @@
 <template>
-  <m-form
-    title="ابطال شماره بارنامه"
-    :showNavigationButtons="false"
-    :showSubmitAndClearForm="false"
-    :showList="false"
-    :canDelete="canDelete"
-    :isEditing="true"
-    :hasClear="false"
-    @submit="revokeLadingBillNumber"
-  >
-    <template>
-      <v-row>
-        <v-col cols="12" md="4">
-          <v-autocomplete
-            :return-object="true"
-            label="سری بارنامه"
-            :items="ladingBillSeriesItems"
-            v-model="ladingBillSeries"
-            item-text="serial"
-            item-value="id"
-            no-filter
-            :search-input.sync="ladingBillSearchInput"
-            clearable
-          ></v-autocomplete>
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-autocomplete
-            :return-object="true"
-            label="شماره بارنامه"
-            :items="ladingBillSeries?ladingBillSeries.numbers:[]"
-            v-model="billNumber"
-            :item-text="(o) => o.number + ' : ' + (o.is_revoked?'باطل شده':'باطل نشده')"
-            item-text="number"
-            item-value="id"
-          ></v-autocomplete>
-        </v-col>
-        <v-col cols="12" md="4" class="mt-2" v-if="billNumber">
-          <p>
-            وضعیت فعلی:
-            <span v-if="billNumber.is_revoked">باطل شده</span>
-            <span v-else>باطل نشده</span>
-          </p>
-        </v-col>
-      </v-row>
-    </template>
-  </m-form>
+  <v-card>
+    <v-card-title>ابطال شماره بارنامه</v-card-title>
+
+    <v-card-text>
+      <m-datatable
+        api-url="dashtbashi/ladingBillNumbers"
+        :headers="headers"
+        :filters.sync="filters"
+      >
+        <template #item.detail="{ item }">
+          <v-btn
+            @click="revokeLadingBillNumber(item, null)"
+            v-if="item.is_revoked"
+            color="blue white--text"
+          >لغو ابطال</v-btn>
+          <v-btn @click="openRevokeDialog(item)" v-else color="red white--text">ابطال</v-btn>
+        </template>
+      </m-datatable>
+    </v-card-text>
+
+    <v-dialog v-model="dialog" max-width="500px" transition="dialog-transition" v-if="item">
+      <v-card>
+        <v-card-title>
+          ابطال شماره بارنامه
+          {{ item.number }}
+          از سری بارنامه
+          {{ item.series?item.series.serial:'' }}
+        </v-card-title>
+
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <date v-model="date" :default="true" />
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="revokeLadingBillNumber(item, date)" color="blue white--text">ابطال</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-card>
 </template>
 
 <script>
 import GetApi from "./GetApi";
+import ListModalFormMixin from "@/components/mcomponents/form/ListModalForm.js";
+import MDatatable from "@/components/mcomponents/datatable/MDatatable";
+
 export default {
-  mixins: [GetApi],
+  mixins: [GetApi, ListModalFormMixin],
+  components: { MDatatable },
   data() {
     return {
-      ladingBillSeries: null,
-      ladingBillSeriesItems: [],
-      ladingBillSearchInput: "",
-      billNumber: null
+      filters: {},
+      item: null,
+      date: null,
+      dialog: false,
     };
   },
-  watch: {
-    ladingBillSearchInput() {
-      this.getLadingBillSeries(this.ladingBillSearchInput);
-    }
+  computed: {
+    headers() {
+      return [
+        {
+          text: "سری بارنامه",
+          value: "series.serial",
+        },
+        {
+          text: "شماره بارنامه",
+          value: "number",
+        },
+        {
+          text: "ابطال شده",
+          value: "is_revoked",
+          type: "boolean",
+        },
+        {
+          text: "تاریخ ابطال",
+          value: "revoked_at",
+        },
+        {
+          text: "",
+          value: "detail",
+        },
+      ];
+    },
   },
   methods: {
-    getLadingBillSeries(search) {
-      this.request({
-        loading: false,
-        url: this.endpoint("dashtbashi/ladingBillSeries/?limit=10&offset=0"),
-        method: "get",
-        params: {
-          serial__icontains: search
-        },
-        success: data => {
-          this.ladingBillSeriesItems = data["results"];
-        }
-      });
+    openRevokeDialog(item) {
+      this.item = item;
+      this.dialog = true;
     },
-    revokeLadingBillNumber(pos) {
+    revokeLadingBillNumber(item, date) {
       this.request({
         url: this.endpoint("dashtbashi/ladingBillNumbers/revoke"),
         method: "post",
         data: {
-          id: this.billNumber.id,
-          is_revoked: !this.billNumber.is_revoked
+          id: item.id,
+          is_revoked: !item.is_revoked,
+          date: date,
         },
-        success: data => {
+        success: (data) => {
           this.successNotify();
-          this.billNumber.is_revoked = !this.billNumber.is_revoked;
-          this.getLadingBillSeries(this.ladingBillSearchInput);
-        }
+          item.is_revoked = !item.is_revoked;
+          if (!item.is_revoked) item.revoked_at = null;
+          this.dialog = false;
+        },
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
