961592561d384fc1cb510cce5464a293e8abf712
Soroosh Morshedi <93315815+Sorooshmorshedi@users.noreply.github.com>
2022-05-09 11:05:52 +0430

    add new filters to incomestate & change export bottuns in balancesheet

--- src/views/panel/balanceSheet/BalanceSheet.vue
+++ src/views/panel/balanceSheet/BalanceSheet.vue
@@ -5,35 +5,39 @@
     <v-card-text>
       <v-row>
         <v-col cols="12" class="d-flex flex-column flex-md-row">
-          <date placeholder="از تاریخ" v-model="filters.from_date" />
-          <date class="mr-md-3 mt-1 mt-md-0" placeholder="تا تاریخ" v-model="filters.to_date" />
+          <date placeholder="از تاریخ" v-model="filters.from_date"/>
+          <date class="mr-md-3 mt-1 mt-md-0" placeholder="تا تاریخ" v-model="filters.to_date"/>
           <v-switch
-            class="mr-md-3 mt-3 mt-md-1"
-            label="در نظر نگرفتن سند بستن"
-            v-model="filters.skip_closing_sanads"
+              class="mr-md-3 mt-3 mt-md-1"
+              label="در نظر نگرفتن سند بستن"
+              v-model="filters.skip_closing_sanads"
           ></v-switch>
           <v-btn
-            :block="isXs"
-            class="blue white--text w-100px mt-3 mr-2 md-5 mt-md-0"
-            @click="getData()"
-          >گزارش</v-btn>
+              :block="isXs"
+              class="blue white--text w-100px mt-3 mr-2 md-5 mt-md-0"
+              @click="getData()"
+          >گزارش
+          </v-btn>
           <v-spacer></v-spacer>
           <v-btn
               :href="this.endpoint('reports/balanceSheet/html') + `?detailed=${detailed == true}&token=${token}`"
               target="_blank"
               class="export-btn mr-1 md-3 mt-5 mt-md-0"
-          >چاپ</v-btn>
+          >چاپ
+          </v-btn>
           <v-btn
               :href="this.endpoint('reports/balanceSheet/pdf') + `?detailed=${detailed == true}&token=${token}`"
               target="_blank"
               class="export-btn mr-1 md-3 mt-5 mt-md-0"
-          >خروجی PDF</v-btn>
+          >خروجی PDF
+          </v-btn>
 
           <v-btn
-            :href="this.endpoint('reports/balanceSheet/xlsx') + `?detailed=${detailed == true}&token=${token}`"
-            target="_blank"
-            class="export-btn mr-1 md-3 mt-5 mt-md-0"
-          >خروجی اکسل</v-btn>
+              :href="this.endpoint('reports/balanceSheet/xlsx') + `?detailed=${detailed == true}&token=${token}`"
+              target="_blank"
+              class="export-btn mr-1 md-3 mt-5 mt-md-0"
+          >خروجی اکسل
+          </v-btn>
 
         </v-col>
 
@@ -41,157 +45,157 @@
           <v-col cols="12" md="6">
             <v-simple-table dense>
               <thead class="grey lighten-3">
-                <tr>
-                  <th colspan="2" class="text-center">دارایی ها</th>
-                </tr>
+              <tr>
+                <th colspan="2" class="text-center">دارایی ها</th>
+              </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th colspan="2" class="text-center">دارایی های جاری</th>
-                </tr>
-                <tr>
-                  <td>{{ d.cacheAndBank.name }}</td>
-                  <td>{{ d.cacheAndBank.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.shortTimeInvestments.name }}</td>
-                  <td>{{ d.shortTimeInvestments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.commercialAccountsAndReceivables.name }}</td>
-                  <td>{{ d.commercialAccountsAndReceivables.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.otherAccountsAndReceivables.name }}</td>
-                  <td>{{ d.otherAccountsAndReceivables.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.inventories.name }}</td>
-                  <td>{{ d.inventories.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.ordersAndPrepayments.name }}</td>
-                  <td>{{ d.ordersAndPrepayments.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.totalCurrentAssets.name }}</td>
-                  <td>{{ d.totalCurrentAssets.remain }}</td>
-                </tr>
-                <tr>
-                  <th colspan="2" class="text-center">دارایی های غیر جاری</th>
-                </tr>
-                <tr>
-                  <td>{{ d.evidentFixedAssets.name }}</td>
-                  <td>{{ d.evidentFixedAssets.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.notEvidentAssets.name }}</td>
-                  <td>{{ d.notEvidentAssets.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.longTimeInvestments.name }}</td>
-                  <td>{{ d.longTimeInvestments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.otherAssets.name }}</td>
-                  <td>{{ d.otherAssets.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.totalNotCurrentAssets.name }}</td>
-                  <td>{{ d.totalNotCurrentAssets.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold blue--text">
-                  <td>{{ d.totalAssets.name }}</td>
-                  <td>{{ d.totalAssets.remain }}</td>
-                </tr>
+              <tr>
+                <th colspan="2" class="text-center">دارایی های جاری</th>
+              </tr>
+              <tr>
+                <td>{{ d.cacheAndBank.name }}</td>
+                <td>{{ d.cacheAndBank.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.shortTimeInvestments.name }}</td>
+                <td>{{ d.shortTimeInvestments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.commercialAccountsAndReceivables.name }}</td>
+                <td>{{ d.commercialAccountsAndReceivables.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.otherAccountsAndReceivables.name }}</td>
+                <td>{{ d.otherAccountsAndReceivables.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.inventories.name }}</td>
+                <td>{{ d.inventories.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.ordersAndPrepayments.name }}</td>
+                <td>{{ d.ordersAndPrepayments.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.totalCurrentAssets.name }}</td>
+                <td>{{ d.totalCurrentAssets.remain }}</td>
+              </tr>
+              <tr>
+                <th colspan="2" class="text-center">دارایی های غیر جاری</th>
+              </tr>
+              <tr>
+                <td>{{ d.evidentFixedAssets.name }}</td>
+                <td>{{ d.evidentFixedAssets.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.notEvidentAssets.name }}</td>
+                <td>{{ d.notEvidentAssets.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.longTimeInvestments.name }}</td>
+                <td>{{ d.longTimeInvestments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.otherAssets.name }}</td>
+                <td>{{ d.otherAssets.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.totalNotCurrentAssets.name }}</td>
+                <td>{{ d.totalNotCurrentAssets.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold blue--text">
+                <td>{{ d.totalAssets.name }}</td>
+                <td>{{ d.totalAssets.remain }}</td>
+              </tr>
               </tbody>
             </v-simple-table>
           </v-col>
           <v-col cols="12" md="6">
             <v-simple-table dense>
               <thead class="grey lighten-3">
-                <tr>
-                  <th colspan="2" class="text-center">بدهی ها و حقوق صاحبان سهام</th>
-                </tr>
+              <tr>
+                <th colspan="2" class="text-center">بدهی ها و حقوق صاحبان سهام</th>
+              </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th colspan="2" class="text-center">بدهی های جاری</th>
-                </tr>
-                <tr>
-                  <td>{{ d.businessAccountsAndPayableDocuments.name }}</td>
-                  <td>{{ d.businessAccountsAndPayableDocuments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.otherAccountsAndPayableDocuments.name }}</td>
-                  <td>{{ d.otherAccountsAndPayableDocuments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.prepayments.name }}</td>
-                  <td>{{ d.prepayments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.saveTypes.name }}</td>
-                  <td>{{ d.saveTypes.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.paidDividends.name }}</td>
-                  <td>{{ d.paidDividends.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.receivableFunds.name }}</td>
-                  <td>{{ d.receivableFunds.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.totalCurrentDebt.name }}</td>
-                  <td>{{ d.totalCurrentDebt.remain }}</td>
-                </tr>
-                <tr>
-                  <th colspan="2" class="text-center">بدهی های غیر جاری</th>
-                </tr>
-                <tr>
-                  <td>{{ d.longtermAccountsAndPayableDocuments.name }}</td>
-                  <td>{{ d.longtermAccountsAndPayableDocuments.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.longtermReceivableFunds.name }}</td>
-                  <td>{{ d.longtermReceivableFunds.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.savedEndingServiceBenefits.name }}</td>
-                  <td>{{ d.savedEndingServiceBenefits.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.totalNotCurrentDebt.name }}</td>
-                  <td>{{ d.totalNotCurrentDebt.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.totalDebt.name }}</td>
-                  <td>{{ d.totalDebt.remain }}</td>
-                </tr>
-                <tr>
-                  <th colspan="2" class="text-center">حقوق صاحبان سهام</th>
-                </tr>
-                <tr>
-                  <td>{{ d.fund.name }}</td>
-                  <td>{{ d.fund.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.savings.name }}</td>
-                  <td>{{ d.savings.remain }}</td>
-                </tr>
-                <tr>
-                  <td>{{ d.accumulatedProfit.name }}</td>
-                  <td>{{ d.accumulatedProfit.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold">
-                  <td>{{ d.equitiesSum.name }}</td>
-                  <td>{{ d.equitiesSum.remain }}</td>
-                </tr>
-                <tr class="font-weight-bold blue--text">
-                  <td>{{ d.totalDebtsAndEquities.name }}</td>
-                  <td>{{ d.totalDebtsAndEquities.remain }}</td>
-                </tr>
+              <tr>
+                <th colspan="2" class="text-center">بدهی های جاری</th>
+              </tr>
+              <tr>
+                <td>{{ d.businessAccountsAndPayableDocuments.name }}</td>
+                <td>{{ d.businessAccountsAndPayableDocuments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.otherAccountsAndPayableDocuments.name }}</td>
+                <td>{{ d.otherAccountsAndPayableDocuments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.prepayments.name }}</td>
+                <td>{{ d.prepayments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.saveTypes.name }}</td>
+                <td>{{ d.saveTypes.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.paidDividends.name }}</td>
+                <td>{{ d.paidDividends.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.receivableFunds.name }}</td>
+                <td>{{ d.receivableFunds.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.totalCurrentDebt.name }}</td>
+                <td>{{ d.totalCurrentDebt.remain }}</td>
+              </tr>
+              <tr>
+                <th colspan="2" class="text-center">بدهی های غیر جاری</th>
+              </tr>
+              <tr>
+                <td>{{ d.longtermAccountsAndPayableDocuments.name }}</td>
+                <td>{{ d.longtermAccountsAndPayableDocuments.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.longtermReceivableFunds.name }}</td>
+                <td>{{ d.longtermReceivableFunds.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.savedEndingServiceBenefits.name }}</td>
+                <td>{{ d.savedEndingServiceBenefits.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.totalNotCurrentDebt.name }}</td>
+                <td>{{ d.totalNotCurrentDebt.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.totalDebt.name }}</td>
+                <td>{{ d.totalDebt.remain }}</td>
+              </tr>
+              <tr>
+                <th colspan="2" class="text-center">حقوق صاحبان سهام</th>
+              </tr>
+              <tr>
+                <td>{{ d.fund.name }}</td>
+                <td>{{ d.fund.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.savings.name }}</td>
+                <td>{{ d.savings.remain }}</td>
+              </tr>
+              <tr>
+                <td>{{ d.accumulatedProfit.name }}</td>
+                <td>{{ d.accumulatedProfit.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold">
+                <td>{{ d.equitiesSum.name }}</td>
+                <td>{{ d.equitiesSum.remain }}</td>
+              </tr>
+              <tr class="font-weight-bold blue--text">
+                <td>{{ d.totalDebtsAndEquities.name }}</td>
+                <td>{{ d.totalDebtsAndEquities.remain }}</td>
+              </tr>
               </tbody>
             </v-simple-table>
           </v-col>
@@ -204,9 +208,10 @@
 <script>
 import date from "@/components/mcomponents/cleave/Date";
 import _ from "lodash";
+
 export default {
   name: "BalanceSheet",
-  components: { date },
+  components: {date},
   props: {
     detailed: {
       default: false,
@@ -265,9 +270,11 @@ export default {
 th {
   text-align: center;
 }
+
 td {
   width: 50%;
 }
+
 tr td:nth-child(2) {
   text-align: left;
   direction: ltr;
--- src/views/panel/incomeStatement/IncomeStatement.vue
+++ src/views/panel/incomeStatement/IncomeStatement.vue
@@ -5,79 +5,101 @@
     <v-card-text>
       <v-row>
         <v-col cols="12" md="2">
-          <date placeholder="از تاریخ" v-model="filters.from_date" />
+          <v-select
+              label="سطح حساب"
+              v-model="filters.level"
+              item-text="text"
+              item-value="value"
+              :items="accountLevels"
+              :return-object="false"
+          />
         </v-col>
         <v-col cols="12" md="2">
-          <date placeholder="تا تاریخ" v-model="filters.to_date" />
+          <v-select
+              :items="accountStatuses"
+              v-model="filters.status"
+              label="وضعیت حساب"
+          />
+        </v-col>
+        <v-col cols="12" md="2">
+          <date placeholder="از تاریخ" v-model="filters.from_date"/>
+        </v-col>
+        <v-col cols="12" md="2">
+          <date placeholder="تا تاریخ" v-model="filters.to_date"/>
         </v-col>
         <v-col cols="12" md="2">
           <v-btn @click="getData()" class="blue white--text w-100px mt-1">گزارش</v-btn>
         </v-col>
-        <v-col cols="6" class="d-flex justify-end">
+        <v-col cols="12" class="d-flex justify-end">
           <v-btn
               :href="this.endpoint('reports/incomeStatement/html') + `?detailed=${detailed == true}&token=${token}`"
               target="_blank"
               class="export-btn ma-1"
-          >چاپ</v-btn>
+          >چاپ
+          </v-btn>
           <v-btn
               :href="this.endpoint('reports/incomeStatement/pdf') + `?detailed=${detailed == true}&token=${token}`"
               target="_blank"
               class="export-btn ma-1"
-          >خروجی PDF</v-btn>
+          >خروجی PDF
+          </v-btn>
           <v-btn
-            :href="this.endpoint('reports/incomeStatement/xlsx') + `?detailed=${detailed == true}&token=${token}`"
-            target="_blank"
-            class="export-btn ma-1"
-          >خروجی اکسل</v-btn>
+              :href="this.endpoint('reports/incomeStatement/xlsx') + `?detailed=${detailed == true}&token=${token}`"
+              target="_blank"
+              class="export-btn ma-1"
+          >خروجی اکسل
+          </v-btn>
 
         </v-col>
         <v-col cols="12">
           <v-simple-table dense>
             <thead class="grey lighten-3">
-              <tr>
-                <th>#</th>
-                <th>شرح</th>
-                <th colspan="2">مبلغ</th>
-              </tr>
+            <tr>
+              <th>#</th>
+              <th>شرح</th>
+              <th colspan="2">مبلغ</th>
+            </tr>
             </thead>
             <tbody>
-              <template v-for="(r, i) in rows">
-                <tr :key="i">
-                  <td>{{ i+1 }}</td>
-                  <td :class="{'font-weight-bold': i == rows.length-1}">
+            <template v-for="(r, i) in rows">
+              <tr :key="i">
+                <td>{{ i + 1 }}</td>
+                <td :class="{'font-weight-bold': i == rows.length-1}">
                     <span
-                      :class="{'red--text': r.type.prefixColor == 'red', 'blue--text': r.type.prefixColor == 'blue'}"
-                      v-if="r.type.hasPrefix"
+                        :class="{'red--text': r.type.prefixColor == 'red', 'blue--text': r.type.prefixColor == 'blue'}"
+                        v-if="r.type.hasPrefix"
                     >{{ r.type.prefix }}</span>
-                    {{ r.type.name }}
-                  </td>
+                  {{ r.type.name }}
+                </td>
 
-                  <td
+                <td
                     :class="{'font-weight-bold': i == rows.length-1}"
                     dir="ltr"
-                  >{{ r.remain | toMoney }}</td>
-                  <td v-if="detailed"></td>
-                </tr>
-                <template v-if="detailed">
-                  <template v-for="(acc, j) in r.accounts">
-                    <tr :key="i + '' + j">
-                      <td></td>
-                      <td class="account-row">{{ acc.title }}</td>
-                      <td class="ltr">{{ acc.remain | toMoney }}</td>
-                      <td></td>
-                    </tr>
-                  </template>
-                  <tr :key="'s' + i">
+                >{{ r.remain | toMoney }}
+                </td>
+                <td v-if="detailed"></td>
+              </tr>
+              <template v-if="detailed">
+                <template v-for="(acc, j) in r.accounts">
+                  <tr :key="i + '' + j">
                     <td></td>
-                    <td class="account-row">جمع {{ r.type.name }}</td>
+                    <td class="account-row">{{ acc.title }}</td>
+                    <td class="ltr">{{ acc.remain | toMoney }}</td>
                     <td></td>
-                    <td
-                      :class="{'text-bold': i == rows.length-1}"
-                      dir="ltr"
-                    >{{ r.remain | toMoney }}</td>
                   </tr>
                 </template>
+                <tr :key="'s' + i">
+                  <td></td>
+                  <td class="account-row">جمع {{ r.type.name }}</td>
+                  <td></td>
+                  <td
+                      :class="{'text-bold': i == rows.length-1}"
+                      dir="ltr"
+                  >{{ r.remain | toMoney }}
+                  </td>
+                </tr>
               </template>
+            </template>
             </tbody>
           </v-simple-table>
         </v-col>
@@ -89,9 +111,10 @@
 <script>
 import date from "@/components/mcomponents/cleave/Date";
 import accountApiMixin from "@/mixin/accountMixin";
+
 export default {
   name: "IncomeStatement",
-  components: { date },
+  components: {date},
   props: {
     detailed: {
       default: false
@@ -100,14 +123,28 @@ export default {
   data() {
     return {
       rows: [],
-      filters: {}
+      filters: {level: 3, status: 'with_transaction'},
+      accountStatuses: [
+        {value: "all", text: "همه"},
+        {value: "with_remain", text: "حساب های دارای مانده"},
+        {value: "with_transaction", text: "حساب های دارای گردش"},
+      ],
+      accountLevels: [
+        {value: 1, text: "کل"},
+        {value: 2, text: "معین"},
+        {value: 3, text: "تفضیلی"},
+      ],
+
+
     };
   },
   created() {
     this.getData();
   },
   updated() {
-    if(this.$route.params.detailed){this.detailed = true}
+    if (this.$route.params.detailed) {
+      this.detailed = true
+    }
 
   },
   methods: {
